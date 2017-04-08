97db4fdb36ca4d78b9f067649a46ba98abd8e6f9
Unknown <j-schirra@gmx.de>
2017-04-08 16:08:11 +0200

    soup heals players

---
+++ src/main/java/mod/upcraftlp/playerluckyblocks/event/SoupHandler.java
@@ -0,0 +1,33 @@
+package mod.upcraftlp.playerluckyblocks.event;
+
+import mod.upcraftlp.playerluckyblocks.Reference;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemSoup;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+
+@EventBusSubscriber(modid = Reference.MODID)
+public class SoupHandler {
+
+    @SubscribeEvent
+    public static void onSoupEaten(PlayerInteractEvent.RightClickItem event) {
+        if(event.getSide() != Side.SERVER || event.getEntityPlayer().isCreative()) return;
+        ItemStack stack = event.getItemStack();
+        if(stack.getItem() instanceof ItemSoup) {
+            ItemSoup item = (ItemSoup) stack.getItem();
+            EntityPlayer player = event.getEntityPlayer();
+            player.heal(item.getHealAmount(stack));
+            player.getFoodStats().addStats(item, stack);
+            player.setHeldItem(event.getHand(), ItemStack.EMPTY);
+            EntityItem itemEntity = new EntityItem(event.getWorld(), player.posX, player.posY, player.posZ, new ItemStack(Items.BOWL));
+            itemEntity.setDefaultPickupDelay();
+            event.getWorld().spawnEntity(itemEntity);
+            event.setCanceled(true);
+        }
+    }
+}
