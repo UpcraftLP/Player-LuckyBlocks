a5da529b3c656dd9d718579710509c37c10e9f0d
Unknown <j-schirra@gmx.de>
2017-04-10 19:34:14 +0200

    fixed players in creative mode not being able to fly

--- src/main/java/mod/upcraftlp/playerluckyblocks/event/FlightHandler.java
+++ src/main/java/mod/upcraftlp/playerluckyblocks/event/FlightHandler.java
@@ -7,7 +7,7 @@ import com.google.common.collect.Lists;
 
 import mod.upcraftlp.playerluckyblocks.Reference;
 import mod.upcraftlp.playerluckyblocks.init.LuckyItems;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
@@ -20,15 +20,18 @@ public class FlightHandler {
     
     @SubscribeEvent
     public static void checkFlight(PlayerTickEvent event) {
-        EntityPlayer player = event.player;
         if(event.side != Side.SERVER) return;
+        EntityPlayerMP player = (EntityPlayerMP) event.player;
         UUID uuid = player.getUniqueID();
+        boolean isNotCreative = !player.capabilities.isCreativeMode;
         if(flightPlayers.contains(uuid)) {
             if(player.inventory.armorInventory.get(2).getItem() != LuckyItems.JETPACK) {
-                player.capabilities.allowFlying = false;
                 flightPlayers.remove(player.getUniqueID());
-                player.capabilities.isFlying = false; //drops player out of the sky
-                player.sendPlayerAbilities(); //sync the change to the client to actually stop the player from flying
+                if(isNotCreative) {
+                    player.capabilities.allowFlying = false;
+                    player.capabilities.isFlying = false; //drops player out of the sky
+                    player.sendPlayerAbilities(); //sync the change to the client to actually stop the player from flying
+                }
             }
             else if(!player.capabilities.allowFlying) {
                 player.capabilities.allowFlying = true;
