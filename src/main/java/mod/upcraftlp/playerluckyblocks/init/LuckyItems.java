57dba952dc05b7ecb7417e652c312155db0bc954
Unknown <j-schirra@gmx.de>
2017-04-10 22:08:31 +0200

    updated item list

--- src/main/java/mod/upcraftlp/playerluckyblocks/init/LuckyItems.java
+++ src/main/java/mod/upcraftlp/playerluckyblocks/init/LuckyItems.java
@@ -1,8 +1,10 @@
 package mod.upcraftlp.playerluckyblocks.init;
 
 import mod.upcraftlp.playerluckyblocks.items.ItemAdminArk;
+import mod.upcraftlp.playerluckyblocks.items.ItemBooster;
 import mod.upcraftlp.playerluckyblocks.items.ItemDagger;
 import mod.upcraftlp.playerluckyblocks.items.ItemDeathNote;
+import mod.upcraftlp.playerluckyblocks.items.ItemLightningBolt;
 import mod.upcraftlp.playerluckyblocks.items.ItemTeleportStaff;
 import mod.upcraftlp.playerluckyblocks.items.ItemWaterOrb;
 import mod.upcraftlp.playerluckyblocks.items.armor.ItemEnderArmor;
@@ -17,17 +19,21 @@ import net.minecraft.item.Item;
 
 public class LuckyItems {
 
+    //TODO: add missing items to drops
+    
 	/**Misc**/
 	public static final Item WATER_ORB = new ItemWaterOrb();
 	public static final Item TELEPORT_STAFF = new ItemTeleportStaff();
 	public static final Item DEATH_NOTE = new ItemDeathNote();
+	public static final Item LIGHTNING_BOLT = new ItemLightningBolt();
+	public static final Item BOOSTER = new ItemBooster(); //missing
 	
 	/**Fruits**/
-	public static final Item FRUIT_FLIGHT = new ItemFruitFlight();
-	public static final Item FRUIT_MAGIC = new ItemFruitMagic();
-	public static final Item FRUIT_DEATH = new ItemFruitDeath();
-	public static final Item FRUIT_GIANT = new ItemFruitGiant();
-	public static final Item FRUIT_WATER = new ItemFruitWater();
+	public static final Item FRUIT_FLIGHT = new ItemFruitFlight(); //missing, unfinished
+	public static final Item FRUIT_MAGIC = new ItemFruitMagic(); //missing, unfinished
+	public static final Item FRUIT_DEATH = new ItemFruitDeath(); //missing
+	public static final Item FRUIT_GIANT = new ItemFruitGiant(); //missing, unfinished
+	public static final Item FRUIT_WATER = new ItemFruitWater(); //missing
 	
 	/**JetPack**/
 	public static final Item JETPACK = new ItemJetPack();
@@ -39,7 +45,7 @@ public class LuckyItems {
 	public static final Item ENDER_BOOTS = new ItemEnderArmor(EntityEquipmentSlot.FEET);
 	
 	/**Swords**/
-	public static final Item ADMIN_ARK = new ItemAdminArk();
-	public static final Item DAGGER = new ItemDagger();
+	public static final Item ADMIN_ARK = new ItemAdminArk(); //missing, unfinished
+	public static final Item SHADOW_DAGGER = new ItemDagger();
 	
 }
